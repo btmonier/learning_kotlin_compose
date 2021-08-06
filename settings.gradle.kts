a14e4111e01d79d9382ce43a873bea89f1baafdd
Brandon <brandon.monier@gmail.com>
2021-08-06 17:29:50 -0400

    Change root name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,5 +4,5 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
-rootProject.name = "learning_kotlin_compose"
+rootProject.name = "compose_web_test"
 
